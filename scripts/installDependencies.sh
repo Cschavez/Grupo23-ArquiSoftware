38be66bd658edeb35977310859e7cd7088fa71bb
Cschavez <cschavez@uc.cl>
2020-11-09 20:36:16 -0300

    1.9.0: new scripts

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo docker-compose -f /home/ubuntu/Grupo23-ArquiSoftware/docker-compose.yaml down
+sudo systemctl stop docker_boot.service
 sudo docker-compose -f /home/ubuntu/Grupo23-ArquiSoftware/docker-compose.yaml build
+sudo systemctl enable
+sudo docker_boot.service
--- scripts/stopServers.sh
+++ scripts/stopServers.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo docker-compose -f /home/ubuntu/Grupo23-ArquiSoftware/docker-compose.yaml down
+sudo systemctl stop
