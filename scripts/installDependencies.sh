fa66a28b88250dd469d54ccb4054e49e56f0871e
Domingo Ramirez <djramirez@uc.cl>
2020-11-12 19:50:31 -0300

    ls

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo docker-compose -f /home/ubuntu/Grupo23-ArquiSoftware/docker-compose.yaml down
 sudo docker-compose -f /home/ubuntu/Grupo23-ArquiSoftware/docker-compose.yaml build
-chmod +x init-letsencrypt.sh
-sudo ./init-letsencrypt.sh 
+sudo ls
