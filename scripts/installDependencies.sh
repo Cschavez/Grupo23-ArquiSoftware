b78fdd43cc8c701e53d3b2ccd0771c1d15c4114d
Domingo Ramirez <djramirez@uc.cl>
2020-11-12 20:35:18 -0300

    ls 2

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo docker-compose -f /home/ubuntu/Grupo23-ArquiSoftware/docker-compose.yaml down
 sudo docker-compose -f /home/ubuntu/Grupo23-ArquiSoftware/docker-compose.yaml build
-sudo ls
