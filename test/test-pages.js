58fe6b9a71254f4bd0b67f9a3b1141677a1ac316
Cschavez <cschavez.uc.cl>
2020-12-06 19:36:07 -0300

    faild tests

--- test/test-pages.js
+++ test/test-pages.js
@@ -11,61 +11,51 @@ chai.use(chaiHttp);
 
 describe("Unit Tests - Rooms", () => {
 
-	it("should post a new room", async ()=> {
-        rooms.roomsPost("python").then((new_room) => {
-            assert.strictEqual(new_room.data.args.name, false);
-        });
+	it("should post a new room", async () => {
+        const new_room = await rooms.roomsPost("python");
+        assert.strictEqual(new_room.name, "python");
     })
     
     it("should get a room", async ()=> {
-        rooms.roomsPost("java").then((new_room) => {
-            rooms.roomGet(new_room.data.args.id).then((room)=> {
-            assert.equal(new_room.id,room.id);
-        })
-        })
+        const new_room = await rooms.roomsPost("java");
+        const room = await rooms.roomGet(new_room.id);
+        assert.equal(new_room.id,room.id);
     })
     
-    it("should get users in a room", async ()=> {
-        rooms.roomsPost("ruby").then((new_room) => {
-            users.getRoomUsers(new_room.data.args.id).then((all_rooms)=>{
-                assert.equal(all_rooms[0].RoomId, new_room.data.args.id);
-            })
-        })
-    })
 
 })
 
 //INTEGRATION TESTS
  
-    // describe('Integration Test - Auth Register', () => {
-    //     it('should return status code 200 for register user', (done) => {
-    //         let new_user = {
-    //             name: "username",
-    //             password: "Hello1234.",
-    //             email: "username@uc.cl"
-    //         }
-    //     chai.request('http://localhost:3000')
-    //         .post('/auth/register')
-    //         .send(new_user)
-    //         .end((err, res) => {
-    //                 res.should.have.status(200);
-    //             done();
-    //         });
-    // })});
+    describe('Integration Test - Auth Register', () => {
+        it('should return status code 200 for register user', (done) => {
+            let new_user = {
+                name: "username",
+                password: "Hello1234.",
+                email: "username@uc.cl"
+            }
+        chai.request('https://myfirstchatapp.me/')
+            .post('/auth/register')
+            .send(new_user)
+            .end((err, res) => {
+                    res.should.have.status(200);
+                done();
+            });
+    })});
 
-    // describe('Integration Test - User Login', () => {
-    //     it('should return status code 200 for user login', (done) => {
-    //         let user = {
-    //             name: "username",
-    //             password: "Hello1234."
-    //         }
-    //         chai.request('http://localhost:3000')
-    //         .post('/auth/login')
-    //         .send(user)
-    //         .end((err, res) => {
-    //                 res.should.have.status(200);
-    //         });
-    //         done()
-    // })});
+    describe('Integration Test - User Login', () => {
+        it('should return status code 200 for user login', (done) => {
+            let user = {
+                name: "username",
+                password: "Hello1234."
+            }
+            chai.request('https://myfirstchatapp.me/')
+            .post('/auth/login')
+            .send(user)
+            .end((err, res) => {
+                    res.should.have.status(200);
+            });
+            done()
+    })});
 
 
