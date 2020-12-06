f7e0f468ec6297a9638bc2a3888e49e810080621
Cschavez <37157327+Cschavez@users.noreply.github.com>
2020-12-06 19:36:50 -0300

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -13,7 +13,7 @@ describe("Unit Tests - Rooms", () => {
 
 	it("should post a new room", async () => {
         const new_room = await rooms.roomsPost("python");
-        assert.strictEqual(new_room.name, "python");
+        assert.strictEqual(new_room.name, "prueba");
     })
     
     it("should get a room", async ()=> {
